2b9d2d461cfc62781df49cacb3d803febccf09a7
joaoRocha47196 <joaocrocha2001@gmail.com>
2023-10-12 21:09:08 +0100

    Populate LAB_03

--- 02_Aulas_Praticas/03/scripts_original/_go.bat
+++ 02_Aulas_Praticas/03/scripts_original/_go.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 :: [PTS: AJUSTAR]
-set psqlPath="C:\Program Files\PostgreSQL\14\bin"
+set psqlPath="C:\myApp\PostgreSQL\10\bin"
 
 :: Base de Dados e nome do utilizador
 SET dataBase=postgres
